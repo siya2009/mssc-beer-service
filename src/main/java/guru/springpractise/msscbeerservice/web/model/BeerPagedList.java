255872fc7d1063d413d97b54690a41ec2aeb7823
Shashi <shashipratap333@gmail.com>
2020-09-28 21:46:01 -0700

    adding jsoncreator example. closes #14

--- src/main/java/guru/springpractise/msscbeerservice/web/model/BeerPagedList.java
+++ src/main/java/guru/springpractise/msscbeerservice/web/model/BeerPagedList.java
@@ -3,7 +3,12 @@ package guru.springpractise.msscbeerservice.web.model;
 import java.util.List;
 
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 
 /** 
  * Created by Pratap on Sep 8, 2020
@@ -12,17 +17,24 @@ import org.springframework.data.domain.Pageable;
 public class BeerPagedList extends PageImpl<BeerDto> {
 
 	
-	public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
-		super(content, pageable, total);
-		
-	}
+	@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BeerPagedList(@JsonProperty("content") List<BeerDto> content,
+                         @JsonProperty("number") int number,
+                         @JsonProperty("size") int size,
+                         @JsonProperty("totalElements") Long totalElements,
+                         @JsonProperty("pageable") JsonNode pageable,
+                         @JsonProperty("last") boolean last,
+                         @JsonProperty("totalPages") int totalPages,
+                         @JsonProperty("sort") JsonNode sort,
+                         @JsonProperty("first") boolean first,
+                         @JsonProperty("numberOfElements") int numberOfElements) {
 
-	
-	public BeerPagedList(List<BeerDto> content) {
-		super(content);
-		
-	}
-	
+        super(content, PageRequest.of(number, size), totalElements);
+    }
+
+    public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
 	
 
 	
